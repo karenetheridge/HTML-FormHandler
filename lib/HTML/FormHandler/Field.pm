29b8f8ca258a5318f85a3a4e0ed90af923c035e8
Gerda Shank <gerda.shank@gmail.com>
2016-10-15 18:22:32 -0400

    fix for deflation of undef value, from ether's pull request 122
    cherry-picked from 631f13c5114 -- actually, I just copied the entire fif
    sub from master since there were conflicts (from other fixes made after
    0.40064).

--- lib/HTML/FormHandler/Field.pm
+++ lib/HTML/FormHandler/Field.pm
@@ -669,26 +669,27 @@ sub fif {
 
     return if ( $self->inactive && !$self->_active );
     return '' if $self->password;
-
+    return unless $result || $self->has_result;
     my $lresult = $result || $self->result;
     if ( ( $self->has_result && $self->has_input && !$self->fif_from_value ) ||
         ( $self->fif_from_value && !defined $lresult->value ) )
     {
         return defined $lresult->input ? $lresult->input : '';
     }
-    if ( $lresult ) {
-        if ( not defined $lresult->value and defined $self->value ) {
-            # this is because checkboxes and submit buttons have their own 'value'
-            # needs to be fixed in some better way
-            return $self->value;
-        }
-
+    if ( $lresult->has_value ) {
+        my $value;
         if( $self->_can_deflate ) {
-            return ($self->_apply_deflation($lresult->value) // '');
+            $value = $self->_apply_deflation($lresult->value);
         }
         else {
-            return ($lresult->value // '');
+            $value = $lresult->value;
         }
+        return ( defined $value ? $value : '' );
+    }
+    elsif ( defined $self->value ) {
+        # this is because checkboxes and submit buttons have their own 'value'
+        # needs to be fixed in some better way
+        return $self->value;
     }
     return '';
 }
