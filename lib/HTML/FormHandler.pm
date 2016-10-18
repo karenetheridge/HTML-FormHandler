e24bde49c08e9054cbaf58769b25a3188a6053c1
Karen Etheridge <ether@cpan.org>
2016-10-18 12:39:45 -0700

    release commit for 0.4006403 "release" for testing

--- lib/HTML/FormHandler.pm
+++ lib/HTML/FormHandler.pm
@@ -25,7 +25,7 @@ use Data::Clone;
 use 5.008;
 
 # always use 5 digits after decimal because of toolchain issues
-our $VERSION = '0.40064';
+our $VERSION = '0.4006403'; # local hack, see my topic/deflation_on_undef_values_release branch
 
 =head1 SYNOPSIS
 
